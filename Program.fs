7f56a73ee6f384de9f69ceaab863655cd1c9435d
Taeseong Jeong <taeseong@kaist.ac.kr>
2025-03-11 21:02:22 +0900

    qqq222

--- Program.fs
+++ Program.fs
@@ -5,7 +5,10 @@
 /// error in processing the inputs, e.g., integer overflows, then the function
 /// should simply return -1 (do not raise an exception).
 let prob1 a b c =
-  failwith "TODO" // REMOVE this line when you implement your own code
+  if (a>=c && b>=c)&&(a>=(-46340) && a<=46340 && b>=(-46340) && b<=46340 && (a*a+b*b)>0) then (a*a+b*b)
+  elif (a>=b && c>=b)&&(a>=(-46340) && a<=46340 && c>=(-46340) && c<=46340 && (a*a+c*c)>0) then (a*a+c*c)
+  elif (b>=a && c>=a)&&(b>=(-46340) && b<=46340 && c>=(-46340) && c<=46340 && (c*c+b*b)>0) then (c*c+b*b)
+  else -1
 
 /// Define a function `prob2` that takes in a string and returns a new string
 /// that ends with a newline character '\n'. The function appends a newline
@@ -17,7 +20,8 @@ let prob1 a b c =
 /// String.length: String.length s returns the length of the string s. Finally,
 /// you can append two strings using the + operator.
 let prob2 (str: string) =
-  failwith "TODO" // REMOVE this line when you implement your own code
+  if String.length str > 0 && str.[String.length str - 1] = '\n' then str
+  else str+"\n"
 
 /// Write a function `prob3` that takes in as input three floating point numbers
 /// a, b, and c, and returns a root of the quadratic formula $ax^2 + bx + c =
@@ -26,7 +30,8 @@ let prob2 (str: string) =
 /// there are no real roots, then the function should return nan, which is a
 /// special floating point number representing "Not a Number".
 let prob3 a b c =
-  failwith "TODO" // REMOVE this line when you implement your own code
+  if b * b - 4.0 * a * c < 0.0 then nan
+  else (-b + sqrt ( b * b - 4.0 * a * c)) / (2.0 * a)
 
 /// Define a function `prob4` that returns the number of days in a month. The
 /// function takes in as input an integer representing a month, and outputs the
@@ -34,7 +39,11 @@ let prob3 a b c =
 /// returns -1 for any error cases. For example, if a number big than 12 is
 /// given as input, then the function should return -1.
 let prob4 month =
-  failwith "TODO" // REMOVE this line when you implement your own code
+  if month < 1 then -1
+  elif month > 12 then -1
+  elif month=4 || month=6 || month=9 || month=11 then 30
+  elif month=2 then 28
+  else 31
 
 [<EntryPoint>]
 let main _args =
